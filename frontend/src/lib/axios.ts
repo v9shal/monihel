6b05412a8991737185b2e48b5675d3b4f4c70e00
vishal sethi <115367234+v9shal@users.noreply.github.com>
2025-07-28 19:42:18 +0530

    Update axios.ts

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000/api';
+const baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:0/api';
 const apiClient=axios.create({
     baseURL:baseURL,
     withCredentials:true,
